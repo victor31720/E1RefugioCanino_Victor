72967f206d7a03d17f6e1862d964b87c00cf5c25
victor31720 <victorgf317@gmail.com>
2024-02-23 15:09:15 +0100

    Ha sido añadido un nuevo perro al refugio

--- RefugioCanino/src/main/java/ed/refugiocanino/MainRefugioCanino.java
+++ RefugioCanino/src/main/java/ed/refugiocanino/MainRefugioCanino.java
@@ -13,6 +13,7 @@ public class MainRefugioCanino {
         // Agregar algunos perros al refugio
         refugio.agregarPerro("Max", "Labrador");
         refugio.agregarPerro("Bella", "Pastor Alemán");
+        refugio.agregarPerro("Son", "Perro de aguas");
 
         // Mostrar la lista de perros en el refugio
         System.out.println("\nLista de perros en el refugio:");
